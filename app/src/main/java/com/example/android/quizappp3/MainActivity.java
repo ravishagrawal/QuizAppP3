6fb7275e7acc1c51fdba8418c2faf85ee1e66fcd
ravishagrawal <agrawal323@yahoo.co.in>
2017-04-26 12:33:50 +0530

    Second commit

--- app/src/main/java/com/example/android/quizappp3/MainActivity.java
+++ app/src/main/java/com/example/android/quizappp3/MainActivity.java
@@ -2,157 +2,167 @@ package com.example.android.quizappp3;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addListenerOnButton();
     }
     /**
      * This method is called when the order button is clicked.
      */
+    private RadioGroup radioSexGroup;
+    private RadioGroup radioQ4Group;
+    private RadioButton radioSexButton;
+    private RadioButton radioQ4Button;
+    private Button btnDisplay;
     int score;
 
-    /**public void increment (View view) {
-        quantity= quantity + 1 ;
-        if (quantity>100) {
-            Toast.makeText(MainActivity.this,
-                    "Quantity cannot be larger than 99", Toast.LENGTH_LONG).show();
 
+    public void onCheckboxClicked(View view) {
+
+
+        // Question 1 Ionic/covalent
+
+        CheckBox question1a, question1b, question1c, question1d;
+
+        question1a= (CheckBox) findViewById(R.id.a1_1_checkbox);
+        question1b= (CheckBox) findViewById(R.id.a1_2_checkbox);
+        question1c= (CheckBox) findViewById(R.id.a1_3_checkbox);
+        question1d= (CheckBox) findViewById(R.id.a1_4_checkbox);
+
+        if (question1a.isChecked() && question1b.isChecked() && question1d.isChecked() && !question1c.isChecked()) {
+            score++;
+            Log.v("MainActivity", "Q1 Score:" + Integer.toString(score));
         }
-        display (quantity);
+         else {
+            score=score;
+        }
+
     }
 
-    public void decrement (View view) {
-        quantity= quantity - 1;
-        if (quantity<0) {
-            quantity=1;
+    public void addListenerOnButton() {
+
+        final RadioButton genderMale, genderFemale, genderRays, genderWaves;
+
+        genderMale = (RadioButton) findViewById(R.id.genderMale);
+        genderFemale = (RadioButton) findViewById(R.id.genderFemale);
+        genderRays = (RadioButton) findViewById(R.id.genderRays);
+        genderWaves = (RadioButton) findViewById(R.id.genderWaves);
+
+        radioSexGroup = (RadioGroup) findViewById(R.id.radioGender);
+        radioQ4Group = (RadioGroup) findViewById(R.id.radioq4);
+        btnDisplay = (Button) findViewById(R.id.btnDisplay);
+
+        btnDisplay.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+                // get selected radio button from radioGroup
+                int selectedId = radioSexGroup.getCheckedRadioButtonId();
+
+                // find the radiobutton by returned id
+                radioSexButton = (RadioButton) findViewById(selectedId);
+
+                // if else loop radio button
+                if(genderMale.isChecked())
+                {
+                    // is checked
+                    score= score+1;
+                    Toast.makeText(MainActivity.this,
+                            "Score +1", Toast.LENGTH_SHORT).show();
+                    Log.v("MainActivity","Radio Score:" +  Integer.toString(score));
+                }
+                else
+                {
+                    // not checked
+                    Toast.makeText(MainActivity.this,
+                            "Response Recorded", Toast.LENGTH_SHORT).show();
+                }
+
+                int selectId = radioQ4Group.getCheckedRadioButtonId();
+
+                // find the radiobutton by returned id
+                radioQ4Button = (RadioButton) findViewById(selectedId);
+
+                // if else loop radio button
+                if(genderWaves.isChecked())
+                {
+                    // is checked
+                    score= score+1;
+                    Toast.makeText(MainActivity.this,
+                            "Score +1", Toast.LENGTH_SHORT).show();
+                    Log.v("MainActivity","Radio Score waves:" +  Integer.toString(score));
+                }
+                else
+                {
+                    // not checked
+                    Toast.makeText(MainActivity.this,
+                            "Response Recorded", Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+
+        });
+
+    }
+
+    public void submit (View view) {
+
+        EditText neutronDisplay = (EditText) findViewById(R.id.neutron);
+        String neutronResult = neutronDisplay.getText().toString();
+        neutronDisplay.setText(neutronResult);
+
+        String sentence = neutronResult;
+        String search = "Neutron";
+
+        if (sentence.toLowerCase().indexOf(search.toLowerCase()) != -1) {
+
+            score = score + 1;
             Toast.makeText(MainActivity.this,
-                    "Quantity cannot be smaller than one", Toast.LENGTH_LONG).show();
+                    "Score +1", Toast.LENGTH_SHORT).show();
+            Log.v("MainActivity", "Input Score:" + Integer.toString(score));
+
+        } else {
+
+            Toast.makeText(MainActivity.this,
+                    "Try Again", Toast.LENGTH_SHORT).show();
+
         }
-        display(quantity);
-    }*/
+    }
 
-    public void submitOrder (View view) {
+    public void getScore (View view) {
 
-        EditText nameDisplay = (EditText)findViewById(R.id.name);
+        EditText nameDisplay = (EditText)findViewById(R.id.name) ;
         String nameResult = nameDisplay.getText().toString();
         nameDisplay.setText(nameResult);
 
-        CheckBox question1a= (CheckBox) findViewById(R.id.a1_1_checkbox);
-        boolean hasAnswer1a= question1a.isChecked();
 
-        CheckBox question1b= (CheckBox) findViewById(R.id.a1_2_checkbox);
-        boolean hasAnswer1b= question1b.isChecked();
+        String quizMessage= scoreSummary(score,nameResult);
+        Toast.makeText(MainActivity.this,
+                quizMessage, Toast.LENGTH_SHORT).show();
 
-        if (hasAnswer1a) {
-            score= score + 1;
-        }
-
-        CheckBox question2a= (CheckBox) findViewById(R.id.a2_1_checkbox);
-        boolean hasAnswer2a= question1a.isChecked();
-
-        CheckBox question2b= (CheckBox) findViewById(R.id.a2_2_checkbox);
-        boolean hasAnswer2b= question1b.isChecked();
-
-        if (hasAnswer2a) {
-            score= score + 1;
-        }
-
-        CheckBox question3a= (CheckBox) findViewById(R.id.a3_1_checkbox);
-        boolean hasAnswer3a= question1a.isChecked();
-
-        CheckBox question3b= (CheckBox) findViewById(R.id.a3_2_checkbox);
-        boolean hasAnswer3b= question1b.isChecked();
-
-        if (hasAnswer3a) {
-            score= score + 1;
-        }
-
-        CheckBox question4a= (CheckBox) findViewById(R.id.a4_1_checkbox);
-        boolean hasAnswer4a= question1a.isChecked();
-
-        CheckBox question4b= (CheckBox) findViewById(R.id.a4_2_checkbox);
-        boolean hasAnswer4b= question1b.isChecked();
-
-        if (hasAnswer4b) {
-            score= score + 1;
-        }
-
-        CheckBox question5a= (CheckBox) findViewById(R.id.a5_1_checkbox);
-        boolean hasAnswer5a= question1a.isChecked();
-
-        CheckBox question5b= (CheckBox) findViewById(R.id.a5_2_checkbox);
-        boolean hasAnswer5b= question1b.isChecked();
-
-        if (hasAnswer5b) {
-            score= score + 1;
-        }
-
-        CheckBox question6a= (CheckBox) findViewById(R.id.a6_1_checkbox);
-        boolean hasAnswer6a= question1a.isChecked();
-
-        CheckBox question6b= (CheckBox) findViewById(R.id.a6_2_checkbox);
-        boolean hasAnswer6b= question1b.isChecked();
-
-        if (hasAnswer6b) {
-            score= score + 1;
-        }
-
-
-        /**int price= baseprice*quantity;
-
-        String priceMessage= createOrderSummary(price,hasWhippedCream,hasChoclate,nameResult);
-        // displayMessage(priceMessage);
-
-        Intent intent = new Intent(Intent.ACTION_SENDTO);
-        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
-        intent.putExtra(Intent.EXTRA_SUBJECT, "Coffee Order Summary");
-        intent.putExtra(Intent.EXTRA_TEXT, priceMessage);
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            startActivity(intent);
-        }*/
-    }
-    /**
-     * This method displays the given quantity value on the screen.
-     */
-    /**private void display (int number) {
-        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
-        quantityTextView.setText("" + number);
-    }*/
-
-
-    /**
-     * Calculates the price of the order.
-     *
-     * @param quantity is the number of cups of coffee ordered
-     */
-    private int calculatePrice(int quantity) {
-        int price = quantity * 5;
-        return price;
     }
 
-    /**private String createOrderSummary(int price, boolean addWhippedCream, boolean addChoclate, String nameInput) {
-        String priceMessage= "Name: " + nameInput;
-        priceMessage= priceMessage + " \nQuantity" + quantity;
-        priceMessage= priceMessage + " \nTotal: $" + price;
-        priceMessage= priceMessage + " \nWhipped Cream: "+ addWhippedCream;
-        priceMessage= priceMessage + " \nChoclate: "+ addChoclate;
-        priceMessage= priceMessage + "\n" + getString(R.string.thank_you);
-        return priceMessage;
-    }*/
+    private String scoreSummary(int score, String nameInput) {
+        String quizMessage= nameInput;
+        quizMessage= quizMessage + " your total score is " + score;
+        return quizMessage;
 
-    /**
-     * This method displays the given text on the screen.
-
-    private void displayMessage(String message) {
-        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
-        orderSummaryTextView.setText(message);
-    }*/
+    }
 }
